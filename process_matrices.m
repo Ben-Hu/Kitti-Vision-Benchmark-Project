695a6abae5bb89196872f4498c1b4bc4e18c06b3
Ben Hu <benjqh@gmail.com>
2016-11-16 19:18:49 -0500

    Update process_matrices.m

--- process_matrices.m
+++ process_matrices.m
@@ -1,8 +1,7 @@
-%Processing Camera Matrices
-clear all; close all;
-CALIB_DIR='/Users/Ben/Desktop/csc420/project/data_road/training/calib/'
-
-test = getMatrix(CALIB_DIR,'P0','um_000000');
+%% Processing Camera Matrices
+%clear all; close all;
+%CALIB_DIR='/Users/Ben/Desktop/csc420/project/data_road/training/calib/'
+%test = getMatrix(CALIB_DIR,'P0','um_000000');
 
 function [P]=getMatrix(calibDir,camera,imageID)
     tarDir = fullfile(calibDir,camera);
@@ -31,23 +30,3 @@ function [P]=getMatrix(calibDir,camera,imageID)
         fclose(fileID);
     end
 end
- Processing Camera Matrices
-clear all; close all;
-CALIB_DIR='/Users/Ben/Desktop/csc420/project/data_road/training/calib/'
-tarDir = fullfile(CALIB_DIR,'P0');
-
-%list of files in given directory
-listing = dir(fullfile(tarDir,'*.txt'));
-
-%for every file, process it
-P = zeros(4,3,size(listing,1));
-for i=1:size(listing,1)
-    filePath = fullfile(tarDir, listing(i).name)
-    fileID = fopen(filePath,'r');
-    formatSpec = '%f';
-    cP = fscanf(fileID,formatSpec);
-    cP = reshape(cP,4,3);
-    P(:,:,i) = cP;
-    fclose(fileID);
-end
-
