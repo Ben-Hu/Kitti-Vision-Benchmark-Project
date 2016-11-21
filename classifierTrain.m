9b67e235a3e013a3151280f9fac2e343d5130dfa
Ben Hu <benjqh@gmail.com>
2016-11-20 22:25:51 -0500

    Update classifierTrain.m

--- classifierTrain.m
+++ classifierTrain.m
@@ -1,40 +1,51 @@
 %% Classifier Training
 clear all; close all;
 globals;
-%% Load training data-set
-origListing = dir(fullfile(TRAIN_ORIG_DIR,'um_*.png'));
-segListing = dir(fullfile(TRAIN_SEG_DIR,'um_road_*.png'));
+%360x1220, 20x20 window
 
-origImgStack = zeros(370,1225);
-segImgStack = zeros(370,1225);
+%% Load training data-set
+trainAll = 1;
+if trainAll
+    origListing = dir(fullfile(TRAIN_ORIG_DIR,'um_000000.png'));
+    segListing = dir(fullfile(TRAIN_SEG_DIR,'um_road_000000.png'));
+    %origListing = dir(fullfile(TRAIN_ORIG_DIR,'*.png'));
+    %segListing = dir(fullfile(TRAIN_SEG_DIR,'*road_*.png'));
+else
+    origListing = dir(fullfile(TRAIN_ORIG_DIR,'um_*.png'));
+    segListing = dir(fullfile(TRAIN_SEG_DIR,'um_road_*.png'));
+end
+
+origImgStack = []; %zeros(360,1220);
+segImgStack = []; %zeros(360,1220);
 
 for i=1:size(origListing,1)
     cImg = rgb2gray(single(imread(fullfile(TRAIN_ORIG_DIR,origListing(i).name)))/255);
-    cImg = cImg(1:370,1:1225);
+    cImg = cImg(1:360,1:1220);
     origImgStack = cat(3,origImgStack,cImg);    
 end
 
 for i=1:size(segListing,1)
     cImg = rgb2gray(single(imread(fullfile(TRAIN_SEG_DIR,segListing(i).name)))/255);
-    cImg = cImg(1:370,1:1225);
+    cImg = cImg(1:360,1:1220);
     segImgStack = cat(3,segImgStack,cImg);
 end
 
 allCV = [];
 idxS = []; %zeros(size(origImgStack,3),1);
 for k=1:size(origImgStack,3)
-    oimg = origImgStack(:,:,i);
-    c = mat2cell(oimg, 37*ones(1,10), 25*ones(1,49));
+    oimg = origImgStack(:,:,k);
+    c = mat2cell(oimg, 20*ones(1,18), 20*ones(1,61));
     cv = zeros(size(c,1)*size(c,2),size(c{1,1},1)*size(c{1,1},2));
     
-    simg = segImgStack(:,:,i);
+    simg = segImgStack(:,:,k);
     rPixVal = max(reshape(simg,1,[]));
     smask = simg(:,:) >= rPixVal;
-    cs = mat2cell(smask, 37*ones(1,10), 25*ones(1,49));
+    cs = mat2cell(smask, 20*ones(1,18), 20*ones(1,61));
     cvs = zeros(size(cs,1)*size(cs,2),size(cs{1,1},1)*size(cs{1,1},2));
-    sMax = rPixVal * 37 * 25;
+    sMax = 1 * 20 * 20; %rPixVal * 20 * 20;
     %sum min = 0;
-   
+    maxcSum = 0;
+    numClass1 = 0;
     %each row of cv corresponds to a vectorized image patch
     for i=1:size(c,1)
         for j=1:size(c,2)
@@ -43,11 +54,17 @@ for k=1:size(origImgStack,3)
             %classify each image patch based on smask sum val
             %take the weighted sum of the pre-segmented training image 
             %classify based on the aggregate pixel value - 
-            cSum = cs{i,j};
-            if (sMax - cSum) < 50 
-                idxS = cat(1,idxS,2);
-            else
+            cSum = sum(reshape(cs{i,j},1,[]));
+            %debug
+            if cSum > maxcSum
+                maxcSum = cSum
+            end
+            
+            if cSum > sMax/2
                 idxS = cat(1,idxS,1);
+                numClass1 = numClass1+1;
+            else
+                idxS = cat(1,idxS,0);
             end
         end
     end
@@ -60,9 +77,10 @@ model = fitcsvm(cv,idx);
 model2 = fitcsvm(allCV,idxS);
 
 % divide a test image with window fn
-xval = rgb2gray(double(imread('data_road/testing/image_2/um_000031.png'))/255);
-xval = xval(1:370,1:1225);
-xc = mat2cell(xval, 37*ones(1,10), 25*ones(1,49));
+xval = rgb2gray(double(imread(fullfile(TRAIN_ORIG_DIR,'um_000000.png')))/255);
+%xval = rgb2gray(double(imread('data_road/testing/image_2/um_000031.png'))/255);
+xval = xval(1:360,1:1220);
+xc = mat2cell(xval, 20*ones(1,18), 20*ones(1,61));
 xcv = zeros(size(xc,1)*size(xc,2),size(xc{1,1},1)*size(xc{1,1},2));
 
 % make vectors from window patches
@@ -73,15 +91,13 @@ for i=1:size(xc,1)
 end
 
 %for each vector in xcv (image patch, classify it)
-labels = zeros(size(xcv,1),1);
-for i=1:size(xcv,1)
-    labels(i) = predict(model2,xcv(i,:));
-end
+labels = predict(model2,xcv);
+
 
 %now that all image patches are classified, assign label value to
 %corresponding block and reconstruct the image
 for ind=1:size(labels,1)
-    [i,j] = ind2sub([size(c,1),size(c,2)],ind)
+    [i,j] = ind2sub([size(c,1),size(c,2)],ind);
     c{i,j}(:,:) = labels(ind);
 end
 res = cell2mat(c);
