7d00d05b83cf4a0d6a1189890c4269947fd1dce8
Ben-Hu <Benjqh@gmail.com>
2017-01-30 12:10:32 -0500

    move code to src

---
+++ src/spsstereo/CMakeFiles/spsstereo.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/Ben/Desktop/git/csc420-project/spsstereo/SGMStereo.cpp" "/Users/Ben/Desktop/git/csc420-project/spsstereo/CMakeFiles/spsstereo.dir/SGMStereo.o"
+  "/Users/Ben/Desktop/git/csc420-project/spsstereo/SPSStereo.cpp" "/Users/Ben/Desktop/git/csc420-project/spsstereo/CMakeFiles/spsstereo.dir/SPSStereo.o"
+  "/Users/Ben/Desktop/git/csc420-project/spsstereo/spsstereo_main.cpp" "/Users/Ben/Desktop/git/csc420-project/spsstereo/CMakeFiles/spsstereo.dir/spsstereo_main.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/spsstereo/CMakeFiles/spsstereo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/spsstereo.dir/SGMStereo.o"
+  "CMakeFiles/spsstereo.dir/SPSStereo.o"
+  "CMakeFiles/spsstereo.dir/spsstereo_main.o"
+  "spsstereo.pdb"
+  "spsstereo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/spsstereo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
